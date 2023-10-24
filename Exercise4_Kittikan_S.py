6ec6615ba3802899ca1a494c37b68609bbd3cfbe
ToRoKunGZ <tor_loorsodsod@hotmail.com>
2023-10-24 13:44:15 +0700

    Create Exercise4_Kittikan_S.py

---
+++ Exercise4_Kittikan_S.py
@@ -0,0 +1,13 @@
+Escore = 20
+''' Foundation English score'''
+Bscore = 15
+''' General Business score'''
+Cscore = 12
+''' Introduction to Computer Systems score'''
+Pscore = 14
+''' Computer Programming score'''
+print("--- Your Score ---")
+print("Foundation English :", Escore)
+print("General Business :", Bscore)
+print("Introduction to Computer Systems :", Cscore)
+print("Computer Programming :", Pscore)
